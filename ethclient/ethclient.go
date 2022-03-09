b6e79e5996ee4838a219fec64a18eaaa57740a47
japson1987 <276286171@qq.com>
2022-03-09 14:58:37 +0800

    Delete redundancy true for getBlockByNumber.

--- ethclient/ethclient.go
+++ ethclient/ethclient.go
@@ -67,7 +67,7 @@ func (ec *Client) Close() {
 // Note that loading full blocks requires two requests. Use HeaderByHash
 // if you don't need all transactions or uncle headers.
 func (ec *Client) BlockByHash(ctx context.Context, hash common.Hash, inclTx bool) (*types.Block, int, error) {
-	return ec.getBlock(ctx, "eth_getBlockByHash", hash, inclTx, true)
+	return ec.getBlock(ctx, "eth_getBlockByHash", hash, inclTx)
 }
 
 // BlockByNumber returns a block from the current canonical chain. If number is nil, the
@@ -76,7 +76,7 @@ func (ec *Client) BlockByHash(ctx context.Context, hash common.Hash, inclTx bool
 // Note that loading full blocks requires two requests. Use HeaderByNumber
 // if you don't need all transactions .
 func (ec *Client) BlockByNumber(ctx context.Context, number *big.Int, inclTx bool) (*types.Block, int, error) {
-	return ec.getBlock(ctx, "eth_getBlockByNumber", toBlockNumArg(number), inclTx, true)
+	return ec.getBlock(ctx, "eth_getBlockByNumber", toBlockNumArg(number), inclTx)
 }
 
 type rpcBlock struct {
@@ -90,7 +90,6 @@ type rpcTransactionNumber struct {
 
 func (ec *Client) getBlock(ctx context.Context, method string, args ...interface{}) (*types.Block, int, error) {
 	log.Info("getBlock...")
-	fmt.Println("getBlock")
 	var raw json.RawMessage
 	err := ec.c.CallContext(ctx, &raw, method, args...)
 	if err != nil {
